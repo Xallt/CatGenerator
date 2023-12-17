f59f4393c12890435bd263f23a575e314b807912
Dmitry <mitya.shabat@gmail.com>
2023-12-17 13:56:49 +0400

    Split into train/val/test

--- data.py
+++ data.py
@@ -4,23 +4,29 @@ from torchvision.io import read_image
 import numpy as np
 from glob import glob
 
+
 class CatDataset(torch.utils.data.Dataset):
     def __init__(self, path, transform=None, seed=42):
         self.path = path
-        self.files = list(glob(os.path.join(path, '*.jpg')))
+        self.files = list(glob(os.path.join(path, "*.jpg")))
         self.transform = transform
 
         self.seed = seed
         self.rng = np.random.default_rng(seed)
 
         self.train_size = int(0.8 * len(self.files))
+        self.val_size = (len(self.files) - self.train_size) // 2
+        self.test_size = len(self.files) - self.train_size - self.val_size
+
         indices = np.arange(len(self.files))
         self.rng.shuffle(indices)
-        self.train_indices = indices[:self.train_size]
-        self.test_indices = indices[self.train_size:]
+        self.train_indices = indices[: self.train_size]
+        self.val_indices = indices[self.train_size : self.train_size + self.val_size]
+        self.test_indices = indices[self.train_size + self.val_size :]
 
     def __len__(self):
         return len(self.files)
+
     def __getitem__(self, idx):
         img = read_image(self.files[idx])
         if self.transform:
@@ -29,14 +35,19 @@ class CatDataset(torch.utils.data.Dataset):
 
     def train_loader(self, batch_size=1):
         return torch.utils.data.DataLoader(
-            self, 
+            self,
             batch_size=batch_size,
-            sampler=torch.utils.data.SubsetRandomSampler(self.train_indices)
+            sampler=torch.utils.data.SubsetRandomSampler(self.train_indices),
+        )
+
+    def val_loader(self, batch_size=1):
+        return torch.utils.data.DataLoader(
+            self,
+            batch_size=batch_size,
+            sampler=torch.utils.data.SubsetRandomSampler(self.val_indices),
         )
 
     def test_loader(self, batch_size=1):
         return torch.utils.data.DataLoader(
-            self, 
-            batch_size=batch_size,
-            sampler=self.test_indices
+            self, batch_size=batch_size, sampler=self.test_indices
         )
